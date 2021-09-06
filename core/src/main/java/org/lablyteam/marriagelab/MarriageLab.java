66f747aebbd343e3b4e5cf4ea8c5e2d7659e6ac9
Pabszito <pabszito@gmail.com>
2021-09-06 01:38:47 -0300

    initial commit

---
+++ core/src/main/java/org/lablyteam/marriagelab/MarriageLab.java
@@ -0,0 +1,16 @@
+package org.lablyteam.marriagelab;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MarriageLab extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}

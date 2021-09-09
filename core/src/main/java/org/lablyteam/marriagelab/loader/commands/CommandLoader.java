af48970e072b99c1ca85d31b69d5e9c91c275bd3
Pabszito <37744843+Pabszito@users.noreply.github.com>
2021-09-09 08:33:16 -0300

    register main command
    i forgot

--- core/src/main/java/org/lablyteam/marriagelab/loader/commands/CommandLoader.java
+++ core/src/main/java/org/lablyteam/marriagelab/loader/commands/CommandLoader.java
@@ -1,6 +1,7 @@
 package org.lablyteam.marriagelab.loader.commands;
 
 import org.lablyteam.marriagelab.MarriageLab;
+import org.lablyteam.marriagelab.commands.MarriageLabCommand;
 import org.lablyteam.marriagelab.commands.MarryCommand;
 import org.lablyteam.marriagelab.internal.CommandClass;
 import org.lablyteam.marriagelab.loader.Loader;
@@ -16,7 +17,8 @@ public class CommandLoader implements Loader {
     @Override
     public void load() {
         registerCommand(
-                new CommandClass("marry", new MarryCommand(plugin))
+                new CommandClass("marry", new MarryCommand(plugin)),
+                new CommandClass("marriagelab", new MarriageLabCommand(plugin))
         );
     }
 
